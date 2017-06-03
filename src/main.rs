15fb8f4935f0e473e1c3d6f70eb3adcf8d59acee
Travis Kaufman <travis.kaufman@gmail.com>
2017-06-03 12:30:17 -0400

    Open image using image crate

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 extern crate image;
 
 use std::env;
-use std::fs::File;
 use std::io;
 use std::io::prelude::*;
 
@@ -12,17 +11,9 @@ fn run_app() -> Result<(), String> {
     }
 
     let fname = &args[1];
-    let mut f = try!(File::open(fname).map_err(|_| "Could not open file"));
-    let mut buf: Vec<u8> = vec![];
-    try!(f.read_to_end(&mut buf).map_err(|_| "Could not read file"));
-
-    match image::guess_format(&buf) {
-        Ok(_) => {
-            println!("Detected type {:?}", image::guess_format(&buf).unwrap());
-            Ok(())
-        }
-        Err(_) => Err(String::from("Unable to detect image type")),
-    }
+    let img = try!(image::open(fname).map_err(|_| "Could not read image")).to_rgb();
+    println!("{:?}", img);
+    Ok(())
 }
 
 fn main() {
